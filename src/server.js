e13705e4269e3d5dd2e3b11d18c3da8599f97547
LaFibreDuDev <yannick.kuhn@oclock.io>
2024-12-22 00:47:47 +0100

    wrapper o2switch

--- src/server.js
+++ src/server.js
@@ -3,12 +3,21 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Wrapper O2SWITCH
+if (typeof(PhusionPassenger) !== 'undefined') {
+    PhusionPassenger.configure({ autoInstall: false });
+}
+
 // Routes
 app.get('/', (req, res) => {
-  res.send('Salut les gens !');
+  res.json({message: 'Salut les gens !'});
 });
 
 // Lancement du serveur
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+if (typeof(PhusionPassenger) !== 'undefined') {
+    app.listen('passenger');
+} else {
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+}
