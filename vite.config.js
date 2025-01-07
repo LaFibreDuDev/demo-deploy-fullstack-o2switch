ce6add9249e926d4af9d494230c9afbec7debd34
LaFibreDuDev <yannick.kuhn@oclock.io>
2025-01-07 23:58:08 +0100

    Changement du build pour rajouter le script reset

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  define: {
+    'process.env.DATABASE_USER': JSON.stringify(process.env.DATABASE_USER),
+    'process.env.DATABASE_PASSWORD': JSON.stringify(process.env.DATABASE_PASSWORD),
+    'process.env.DATABASE_HOST': JSON.stringify(process.env.DATABASE_HOST),
+    'process.env.DATABASE_PORT': JSON.stringify(process.env.DATABASE_PORT),
+    'process.env.DATABASE_SSL': JSON.stringify(process.env.DATABASE_SSL),
+  },
   optimizeDeps: {
     include: ['express'],
   },
   build: {
     outDir: 'dist', 
     lib: {
-        entry: 'src/server.js', 
+        entry: ['src/server.js', 'src/models/reset.js'], 
         formats: ['cjs'], 
     },
     rollupOptions: {
