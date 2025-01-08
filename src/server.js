386ba190b3b421f3a7d51e9f6ccc682bdb5f5268
LaFibreDuDev <yannick.kuhn@oclock.io>
2025-01-08 02:14:35 +0100

    Nouveau test config

--- src/server.js
+++ src/server.js
@@ -72,8 +72,10 @@ app.use(cookieParser());
 app.get('/config', (req, res) => {
   const { dialect, database, user, _, dbhost } = config.database;
   const { url, host, port, secure } = config.server;
-  console.log('server', url, host, port, secure)
-  console.log('db', dialect, database, user, dbhost)
+  res.status(200).json({
+    url, host, port, secure,
+    dialect, database, user, dbhost
+  })
 });
 
 // Authentication routes
